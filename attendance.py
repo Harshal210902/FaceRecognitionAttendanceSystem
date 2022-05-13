2af35dd8331d446ee23c3ce19d2bf1a7afec4689
Harshal <20051462@kiit.ac.in>
2022-05-13 09:46:24 +0530

    Initial Commit

---
+++ attendance.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'Resources'
+images = []
+classnames = []
+mylist = os.listdir(path)
+print(mylist)
+
+for cl in mylist:
+    curimg = cv2.imread(f'{path}/{cl}')
+    images.append(curimg)
+    classnames.append(os.path.splitext(cl)[0])
+
+print(classnames)
+
+def findencodings(images):
+    encodelist = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+    return encodelist
+
+def markattendance(name):
+    with open('attendancename.csv', 'r+') as f:
+        mydatalist = f.readlines()
+        namelist = []
+        for line in mydatalist:
+            entry = line.split(',')
+            namelist.append(entry[0])
+
+        if name not in namelist:
+            now = datetime.now()
+            dtstring = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtstring}')
+
+
+
+
+encodelistknown = findencodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+   success, img = cap.read()
+   imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+   imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+   facecurframe = face_recognition.face_locations(imgs)
+   encodescurframe = face_recognition.face_encodings(imgs, facecurframe)
+
+   for encodeface,faceloc in zip(encodescurframe, facecurframe):
+       matches = face_recognition.compare_faces(encodelistknown, encodeface)
+       facedist = face_recognition.face_distance(encodelistknown, encodeface)
+       # print(facedist)
+       matchindex = np.argmin(facedist)
+
+       if matches[matchindex]:
+           name = classnames[matchindex].upper()
+           # print(name)
+           y1,x2,y2,x1 = faceloc
+           y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+           cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+           cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
+           cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+           markattendance(name)
+
+   cv2.imshow('webcam', img)
+   cv2.waitKey(1)
+
+
+
+
+
+# faceloc = face_recognition.face_locations(imgdhoni)[0]
+# encodedhoni = face_recognition.face_encodings(imgdhoni)[0]
+# cv2.rectangle(imgdhoni, (faceloc[3], faceloc[0]), (faceloc[1], faceloc[2]), (255,0,255), 2)
+#
+# faceloctest = face_recognition.face_locations(imgtest)[0]
+# encodetest = face_recognition.face_encodings(imgtest)[0]
+# cv2.rectangle(imgtest, (faceloctest[3], faceloctest[0]), (faceloctest[1], faceloctest[2]), (255,0,255), 2)
+#
+# results = face_recognition.compare_faces([encodedhoni], encodetest)
+# facedis = face_recognition.face_distance([encodedhoni], encodetest)
